087a967332a0a8eb81ffba9468741d0d42c728ee
hyranno <hyranno4pub@gmail.com>
2023-07-04 12:58:55 +0900

    add shader crate

---
+++ build.rs
@@ -0,0 +1,23 @@
+use std::{fs, path::PathBuf};
+use spirv_builder::{MetadataPrintout, SpirvBuilder, ModuleResult};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let target = "spirv-unknown-spv1.1";
+    let shaders_dir: PathBuf = [std::env::var("CARGO_MANIFEST_DIR")?, String::from("assets/shaders/")].iter().collect();
+    fn copy_spv(src: &PathBuf, dest_dir: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+        let dest: PathBuf = [dest_dir, &PathBuf::from(src.file_name().unwrap())].iter().collect();
+        fs::copy(src, dest)?;
+        Ok(())
+    }
+
+    let shader_crate = "shaders/custom_material";
+    let built_shader = SpirvBuilder::new(shader_crate, target)
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
+    match built_shader.module {
+        ModuleResult::SingleModule(path) => copy_spv(&path, &shaders_dir)?,
+        ModuleResult::MultiModule(tree) => for path in tree.values() { copy_spv(&path, &shaders_dir)? },
+    }
+
+    Ok(())
+}
---
+++ shaders/custom_material/src/lib.rs
@@ -0,0 +1,9 @@
+#![no_std]
+
+use spirv_std::spirv;
+use spirv_std::glam::{vec4, Vec4};
+
+#[spirv(fragment)]
+pub fn main_fs(output: &mut Vec4) {
+    *output = vec4(1.0, 0.0, 0.0, 1.0);
+}
